1cb88cb7aede0042a62384506c7b8aed0fed8a07
falihnaufal17 <falihnaufal1700@gmail.com>
2023-02-11 03:30:57 +0700

    feat: add new portofolio

--- js/portofolio.js
+++ js/portofolio.js
@@ -1,18 +1,23 @@
 let data = [
     {
         name: 'MOLADIN',
-        image: '../images/Screenshot 2021-07-29 at 9.45 1 (1).png',
+        image: '../images/moladindotcom.png',
         url: 'https://moladin.com/'
     },
     {
         name: 'TOKO SULTAN',
         image: '../images/Screenshot 2021-07-29 at 9.45 1 (2).png',
-        url: 'https://tokosultan.netlify.app/'
+        url: 'https://github.com/falihnaufal17/toko-sultan-web'
     },
     {
         name: 'BAZAR',
         image: '../images/Screenshot 2021-07-29 at 9.45 1.png',
         url: 'https://shopbazar.co/'
+    },
+    {
+        name: 'PulsaDong Seller',
+        image: '../images/pulsadong-seller.jpeg',
+        url: 'https://play.google.com/store/apps/details?id=com.pulsadongmobile'
     }
 ]
 let elementList = ''
